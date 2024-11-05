d6eabea952b3a4f9e6b0021e5d320fdf3935d654
OykuPam <ec24170@qmul.ac.uk>
2024-11-05 20:18:57 +0000

    Added missing cards

--- src/main/java/players/heuristics/AdjustedScoreHeuristic.java
+++ src/main/java/players/heuristics/AdjustedScoreHeuristic.java
@@ -41,6 +41,7 @@ public class AdjustedScoreHeuristic implements IStateHeuristic {
             heuristicValue += evaluatePlayerHandContents(sgState, playerId) * playerWeight; // weighted contrubition from players cards.
             heuristicValue += evaluateOpponentHandContents(sgState, playerId) * opponentWeight; // weighted contrubution from opponents cards.
             heuristicValue += evaluatePlayedCards(sgState, playerId) * 0.5; //evaluation of played cards
+            heuristicValue += evaluateStrategy(sgState, playerId);//for collection strategy
         }
 
         return heuristicValue;
@@ -55,11 +56,21 @@ public class AdjustedScoreHeuristic implements IStateHeuristic {
         int makiPoints = pointsPerCardType.getOrDefault(SGCard.SGCardType.Maki, new Counter()).getValue();
         int tempuraPoints = pointsPerCardType.getOrDefault(SGCard.SGCardType.Tempura, new Counter()).getValue();
         int sashimiPoints = pointsPerCardType.getOrDefault(SGCard.SGCardType.Sashimi, new Counter()).getValue();
+        int squidNigiriPoints = pointsPerCardType.getOrDefault(SGCard.SGCardType.SquidNigiri, new Counter()).getValue();
+        int salmonNigiriPoints = pointsPerCardType.getOrDefault(SGCard.SGCardType.SalmonNigiri, new Counter()).getValue();
+        int eggNigiriPoints = pointsPerCardType.getOrDefault(SGCard.SGCardType.EggNigiri, new Counter()).getValue();
+        int wasabiPoints = pointsPerCardType.getOrDefault(SGCard.SGCardType.Wasabi, new Counter()).getValue();
+        int chopsticksPoints = pointsPerCardType.getOrDefault(SGCard.SGCardType.Chopsticks, new Counter()).getValue();
         int puddingPoints = pointsPerCardType.getOrDefault(SGCard.SGCardType.Pudding, new Counter()).getValue();
 
         if (makiPoints >= 5) bonus += 3;
-        if (tempuraPoints >= 5) bonus += 2;
-        if (sashimiPoints >= 10) bonus += 5;
+        if (tempuraPoints % 2 == 0 && tempuraPoints > 0) bonus += (tempuraPoints / 2) * 5; // Pair bonus for Tempura
+        if (sashimiPoints % 3 == 0 && sashimiPoints > 0) bonus += (sashimiPoints / 3) * 10; // Triplet bonus for Sashimi
+        if (salmonNigiriPoints > 0) bonus += salmonNigiriPoints * 2;
+        if (squidNigiriPoints > 0) bonus += squidNigiriPoints * 3;
+        if (eggNigiriPoints > 0) bonus += eggNigiriPoints;
+        if (wasabiPoints > 0) bonus += wasabiPoints * 2;
+        if (chopsticksPoints > 0) bonus += chopsticksPoints;
         if (puddingPoints >= 5) bonus += 3;
 
         return bonus;
@@ -74,6 +85,11 @@ public class AdjustedScoreHeuristic implements IStateHeuristic {
         int makiCount = 0;
         int tempuraCount = 0;
         int sashimiCount = 0;
+        int squidNigiriCount = 0;
+        int salmonNigiriCount = 0;
+        int eggNigiriCount = 0;
+        int wasabiCount = 0;
+        int chopsticksCount = 0;
         int puddingCount = 0;
 
 
@@ -82,7 +98,11 @@ public class AdjustedScoreHeuristic implements IStateHeuristic {
                 case Maki -> makiCount++;
                 case Tempura -> tempuraCount++;
                 case Sashimi -> sashimiCount++;
+                case EggNigiri -> eggNigiriCount++;
+                case SquidNigiri -> squidNigiriCount++;
+                case SalmonNigiri -> salmonNigiriCount++;
                 case Pudding -> puddingCount++;
+
 
             }
         }
@@ -90,6 +110,9 @@ public class AdjustedScoreHeuristic implements IStateHeuristic {
         if (makiCount >= 1) handBonus += makiCount * 2; //more maki increases the score
         if (tempuraCount == 1) handBonus += 3;
         if (sashimiCount == 2) handBonus += 6;
+        if (squidNigiriCount >= 1) handBonus += squidNigiriCount * 3; //squid has higher value
+        if (wasabiCount >= 1) handBonus += wasabiCount * 2; // wasabi increases nigiri score
+        if (chopsticksCount >= 1) handBonus += chopsticksCount * 1; // chopsticks provide flexibility
 
         return handBonus;
 
@@ -102,6 +125,11 @@ public class AdjustedScoreHeuristic implements IStateHeuristic {
         int makiCount = 0;
         int tempuraCount = 0;
         int sashimiCount = 0;
+        int squidNigiriCount = 0;
+        int salmonNigiriCount = 0;
+        int eggNigiriCount = 0;
+        int wasabiCount = 0;
+        int chopsticksCount = 0;
         int puddingCount = 0;
 
         for (int opponentId = 0; opponentId < sgState.getNPlayers(); opponentId++){
@@ -113,6 +141,11 @@ public class AdjustedScoreHeuristic implements IStateHeuristic {
                         case Maki -> makiCount++;
                         case Tempura -> tempuraCount++;
                         case Sashimi -> sashimiCount++;
+                        case SquidNigiri -> squidNigiriCount++;
+                        case SalmonNigiri -> salmonNigiriCount++;
+                        case EggNigiri -> eggNigiriCount++;
+                        case Wasabi -> wasabiCount++;
+                        case Chopsticks -> chopsticksCount++;
                         case Pudding -> puddingCount++;
                     }
                 }
@@ -121,6 +154,10 @@ public class AdjustedScoreHeuristic implements IStateHeuristic {
         if (makiCount >= 1) handBonus += makiCount * 2; //more maki increases the score
         if (tempuraCount == 1) handBonus +=3;
         if (sashimiCount == 2) handBonus +=6;
+        if (squidNigiriCount >= 1) handBonus += squidNigiriCount * 3; //squid has higher value
+        if (wasabiCount >= 1) handBonus += wasabiCount * 2; // wasabi increases nigiri score
+        if (chopsticksCount >= 1) handBonus += chopsticksCount * 1; // chopsticks provide flexibility
+
 
         return handBonus;
     }
@@ -134,6 +171,11 @@ public class AdjustedScoreHeuristic implements IStateHeuristic {
         int tempuraPlayed = 0;
         int sashimiPlayed = 0;
         int makiPlayed = 0;
+        int squidNigiriPlayed = 0;
+        int salmonNigiriPlayed = 0;
+        int eggNigiriPlayed = 0;
+        int wasabiPlayed = 0;
+        int chopsticksPlayed = 0;
 
 
         for (SGCard card : playerPlayedCards.getComponents()){
@@ -141,14 +183,68 @@ public class AdjustedScoreHeuristic implements IStateHeuristic {
                 case Tempura -> tempuraPlayed++;
                 case Sashimi -> sashimiPlayed++;
                 case Maki -> makiPlayed++;
+                case SquidNigiri -> squidNigiriPlayed++;
+                case SalmonNigiri -> salmonNigiriPlayed++;
+                case EggNigiri -> eggNigiriPlayed++;
+                case Wasabi -> wasabiPlayed++;
+                case Chopsticks -> chopsticksPlayed++;
             }
         }
 
         if (tempuraPlayed >= 2) playedCardsBonus += 5;
         if (sashimiPlayed >= 3) playedCardsBonus += 10;
-        if (makiPlayed >= 3) playedCardsBonus += makiPlayed * 1.5;
+        if (makiPlayed >= 3) playedCardsBonus += makiPlayed * 1.5;// for multiple
+        if (squidNigiriPlayed >= 1) playedCardsBonus += squidNigiriPlayed * 3;
+        if (salmonNigiriPlayed >= 1) playedCardsBonus += salmonNigiriPlayed * 2;
 
         return playedCardsBonus;
+    }
+
+    private double evaluateStrategy(SGGameState sgState, int playerId){
+        double collectionBonus = 0.0;
+
+        Map<SGCard.SGCardType, Counter>[] pointsPerCardTypeAll = sgState.getPointsPerCardType();
+        Map<SGCard.SGCardType, Counter> pointsPerCardType = pointsPerCardTypeAll[playerId];
+        int[] totalCardCounts = new int[SGCard.SGCardType.values().length];
+
+        for ( int i = 0;i < sgState.getNPlayers(); i++){
+            if ( i != playerId){
+                for (SGCard.SGCardType cardType : SGCard.SGCardType.values()){
+                    totalCardCounts[cardType.ordinal()] += pointsPerCardTypeAll[i].getOrDefault(cardType, new Counter()).getValue();
+
+                }
+            }
+        }
+
+        int playerMakiCount = pointsPerCardType.getOrDefault(SGCard.SGCardType.Maki, new Counter()).getValue();
+        if (playerMakiCount > totalCardCounts[SGCard.SGCardType.Maki.ordinal()]) {
+            collectionBonus += 5; // Bonus for having more Maki rolls than any other player
+        }
+
+        int playerPuddingCount = pointsPerCardType.getOrDefault(SGCard.SGCardType.Pudding, new Counter()).getValue();
+        if (playerPuddingCount > totalCardCounts[SGCard.SGCardType.Pudding.ordinal()]) {
+            collectionBonus += 3; // Bonus for having more Pudding cards
+        }
+
+        int playerTempuraCount = pointsPerCardType.getOrDefault(SGCard.SGCardType.Tempura, new Counter()).getValue();
+        if (playerTempuraCount % 2 == 0 && playerTempuraCount > 0) {
+            collectionBonus += (playerTempuraCount / 2) * 5; // Pair bonus for Tempura
+        }
+
+        int playerSashimiCount = pointsPerCardType.getOrDefault(SGCard.SGCardType.Sashimi, new Counter()).getValue();
+        if (playerSashimiCount % 3 == 0 && playerSashimiCount > 0) {
+            collectionBonus += (playerSashimiCount / 3) * 10; // Triplet bonus for Sashimi
+        }
+
+
+        int playerWasabiCount = pointsPerCardType.getOrDefault(SGCard.SGCardType.Wasabi, new Counter()).getValue();
+        collectionBonus += playerWasabiCount * 2; // Bonus for Wasabi
+
+        int playerChopsticksCount = pointsPerCardType.getOrDefault(SGCard.SGCardType.Chopsticks, new Counter()).getValue();
+        collectionBonus += playerChopsticksCount * 1; // Bonus for Chopsticks
+
+        return collectionBonus;
+
     }
 
 
